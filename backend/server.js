4a2ae7384da36c9e22c789747c9a2c2babc4dc7c
Ben Walton <ben.wallos901@gmail.com>
2019-08-08 12:19:34 +1000

    Update express server to use routes to reference the database

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,17 @@ connection.once("open", () => {
     console.log("MongoDB database connection established successfully.");
 });
 
+const usersRouter = require('./routes/users');
+const postsRouter = require('./routes/posts');
+const commentsRouter = require('./routes/comments');
+const statusRouter = require('./routes/status');
+const user_statusRouter = require('./routes/user_status');
 
+app.use('/users', usersRouter);
+app.use('/posts', postsRouter);
+app.use('/comments', commentsRouter);
+app.use('/status', statusRouter);
+app.use('/user_status', user_statusRouter);
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
