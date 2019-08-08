533fb8d5d1400e357b217143382fb5c466c7534f
Ben Walton <ben.wallos901@gmail.com>
2019-08-08 11:52:16 +1000

    Add database models using mongoose.js for all entities

---
+++ backend/models/comment.model.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const commentSchema = new Schema({
+    content: { type: String, required: true },
+    alt_text: { type: String, required: true },
+    created_at: { type: Date, required: true },
+    user_id: { type: String, required: true },
+    post_id: { type: String, required: true },
+    status_id: { type: String, required: true }
+}, {
+    timestamps: true,
+});
+
+const Comment = mongoose.model('Comment', commentSchema);
+
+module.exports = Comment;
