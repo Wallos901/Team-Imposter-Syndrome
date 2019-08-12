66645a1f040d247b0438bb6e2cd726123336c777
Josh Nairn <Joshua.M.Nairn@student.uts.edu.au>
2019-08-12 13:20:58 +1000

    Partially add navbar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem } from 'reactstrap';
+
+import NavbarComp from "./components/navbar.component";
 
 function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <Router>
+      <div className="container">
+        <Navbar/>
+      </div>
+    </Router>
   );
 }
 
