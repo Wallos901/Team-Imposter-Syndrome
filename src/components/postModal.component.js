2e88ea180cce261d4b5bdad1623b143fc30a91f3
Madeleine Mackey <mackeymadeleine@gmail.com>
2019-08-20 13:48:03 +1000

    Fixed postModal toggling

--- src/components/postModal.component.js
+++ src/components/postModal.component.js
@@ -5,30 +5,30 @@ export default class PostModalComp extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-          postModalComp: false
+          showPostModal: false
         };
     
-        this.togglePostModalComp = this.togglePostModalComp.bind(this);
+        this.togglePostModal = this.togglePostModal.bind(this);
       }
     
-    togglePostModalComp() {
-        this.setState(prevState => ({
-            postModalComp: !prevState.PostModalComp
+    togglePostModal() {
+        this.setState(() => ({
+            showPostModal: !this.state.showPostModal
         }));
       }
 
   render() {
     return (
       <div>
-          <Button onClick={this.togglePostModalComp}>Show Modal</Button>
-          <Modal isOpen={this.state.postModalComp} toggle={this.togglePostModalComp}>
+          <Button onClick={this.togglePostModal}>Show Modal</Button>
+          <Modal isOpen={this.state.showPostModal} toggle={this.togglePostModal}>
           <ModalHeader>Modal title</ModalHeader>
           <ModalBody>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
           </ModalBody>
           <ModalFooter>
-            <Button color="primary" onClick={this.togglePostModalComp}>Do Something</Button>{' '}
-            <Button color="secondary" onClick={this.togglePostModalComp}>Cancel</Button>
+            <Button color="primary" onClick={this.togglePostModal}>Do Something</Button>{' '}
+            <Button color="secondary" onClick={this.togglePostModal}>Cancel</Button>
           </ModalFooter>
         </Modal>
       </div>
