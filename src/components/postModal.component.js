e36334f0fcfdfd6ec21937dd6f4d3d59bd16af1b
Josh Nairn <12876393@student.uts.edu.au>
2019-08-20 16:25:11 +1000

    Refactor cards into single card module

--- src/components/postModal.component.js
+++ src/components/postModal.component.js
@@ -1,37 +1,54 @@
-import React from 'react';
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import React from "react";
+import {
+    Button,
+    Modal,
+    ModalBody,
+    ModalFooter,
+    CardImg
+} from "reactstrap";
+
+import "../styling.css";
 
 export default class PostModalComp extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-          showPostModal: false
+            showPostModal: false
         };
-    
+
         this.togglePostModal = this.togglePostModal.bind(this);
-      }
-    
+    }
+
     togglePostModal() {
         this.setState(() => ({
             showPostModal: !this.state.showPostModal
         }));
-      }
+    }
 
-  render() {
-    return (
-      <div>
-          <Button onClick={this.togglePostModal}>Show Modal</Button>
-          <Modal isOpen={this.state.showPostModal} toggle={this.togglePostModal}>
-          <ModalHeader>Modal title</ModalHeader>
-          <ModalBody>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-          </ModalBody>
-          <ModalFooter>
-            <Button color="primary" onClick={this.togglePostModal}>Do Something</Button>{' '}
-            <Button color="secondary" onClick={this.togglePostModal}>Cancel</Button>
-          </ModalFooter>
-        </Modal>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <CardImg
+                    onClick={this.togglePostModal}
+                    top
+                    width="100%"
+                    src={this.props.imageUrl}
+                    alt="image goes here"
+                />
+                <Modal size={"xl"} isOpen={this.state.showPostModal} toggle={this.togglePostModal}>
+                    <ModalBody class={"modal-body"}>
+                        <img src={this.props.imageUrl} alt={"some alt text"}/>
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button color="primary">
+                            Reply
+                        </Button>
+                        <Button color="secondary" onClick={this.togglePostModal}>
+                            Close
+                        </Button>
+                    </ModalFooter>
+                </Modal>
+            </div>
+        );
+    }
 }
