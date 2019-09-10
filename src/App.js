da19b5a440f11eb1de526e98ec0e2f63fb59986b
Ben Walton <ben.wallos901@gmail.com>
2019-09-10 17:13:40 +1000

    Revert "Add redux boilerplate and update feed component to work with redux"
    This reverts commit b94a9b5b051d53f92ba66e6ed806aa60ee7a0330.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import store from './store';
-
 import HomepageComp from "./components/homepage.component";
 
 function App() {
   return (
-    <Provider store={store}>
-      <HomepageComp />
-    </Provider>
+    <HomepageComp/>
   );
 }
 
