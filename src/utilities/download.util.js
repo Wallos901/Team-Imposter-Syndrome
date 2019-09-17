4054f401e8d3daa9aed4539772eccac1cd9667d6
Madeleine Mackey <mackeymadeleine@gmail.com>
2019-09-17 23:30:14 +1000

    Remove S3 config from download util

--- src/utilities/download.util.js
+++ src/utilities/download.util.js
@@ -1,15 +1,6 @@
-// Utility function to download to the database and S3 bucket.
+// Utility function to download using routes.
 
 import axios from "axios";
-
-// Configuration for the S3 bucket - "ap-southeast-2" is the code for Asia Pacific (Australia) - accessKeyId and secretAccessKey are brought in from the .env file.
-const config = {
-    bucketName: "discussion-board",
-    dirName: "photos",
-    region: "ap-southeast-2",
-    accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY,
-    secretAccessKey: process.env.REACT_APP_AWS_SECRET_KEY
-};
 
 export async function getAll(type) {
     return new Promise((resolve) => {
