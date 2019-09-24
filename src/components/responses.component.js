18712ee75651fbb592d55018bec9ea564c8bc967
Josh Nairn <12876393@student.uts.edu.au>
2019-09-24 14:32:26 +1000

    Add template comment structure and images

--- src/components/responses.component.js
+++ src/components/responses.component.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import {Media} from "reactstrap";
 
 export default class Responses extends React.Component {
     render() {
@@ -8,32 +7,33 @@ export default class Responses extends React.Component {
                 <hr/>
                 <h3>Responses</h3>
                 <hr/>
-                <Media list>
-                    <Media tag="li">
-                        <Media body>
-                            Image 1
-                            <Media>
-                                <Media left>
-                                    <Media object data-src="" alt="-------------"/>
-                                </Media>
-                                <Media body>
-                                    Image 1.1
-                                </Media>
-                            </Media>
-                        </Media>
-                        <Media body>
-                            Image 2
-                            <Media>
-                                <Media left>
-                                    <Media object data-src="" alt="-------------"/>
-                                </Media>
-                                <Media body>
-                                    Image 2.1
-                                </Media>
-                            </Media>
-                        </Media>
-                    </Media>
-                </Media>
+
+                <ul className={"comment-list-parent"}>
+                    <li><img className={"comment-post"}
+                             src={"https://www.abc.net.au/radionational/image/6289622-4x3-340x255.png"}/>
+                        <ul className={"comment-list"}>
+                            <li><img className={"comment-post"}
+                                     src={"http://www.cairowestmag.com/wp-content/uploads/2018/09/Orange.jpg"}/>
+                                <ul className={"comment-list"}>
+                                    <li><img className={"comment-post"}
+                                             src={"https://colourlex.com/wp-content/uploads/2014/12/bleizinngelb-farbbuch-opt.jpg"}/>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li><img className={"comment-post"}
+                                     src={"https://cdn.shopify.com/s/files/1/0882/1686/products/1_f87fbb63-1587-40ad-b2df-57e94acfa608.jpg?v=1493726820"}/>
+                            </li>
+                        </ul>
+                    </li>
+                    <hr/>
+                    <li><img className={"comment-post"}
+                             src={"https://i.guim.co.uk/img/static/sys-images/Guardian/Pix/pictures/2013/8/1/1375354802439/Blue---the-colour-008.jpg?width=300&quality=85&auto=format&fit=max&s=2a7663fea4602034647ecb92ae228d96"}/>
+                        <ul className={"comment-list"}>
+                            <li><img className={"comment-post"}
+                                     src={"https://miro.medium.com/max/400/1*bNfxs62uJzISTfuPlOzOWQ.png"}/></li>
+                        </ul>
+                    </li>
+                </ul>
             </div>
         );
     }
