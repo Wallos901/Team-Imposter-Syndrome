9724901577b88e14c4f0e490714f02b6581d1c07
Wallos901 <ben.wallos901@gmail.com>
2019-10-02 09:59:24 +1000

    Allow a category to be selected on image upload

--- backend/models/post.model.js
+++ backend/models/post.model.js
@@ -22,6 +22,10 @@ const postSchema = new Schema({
         type: Map,
         of: Number,
         default: { "like": 0, "dislike": 0, "love": 0, "fire": 0 }
+    },
+    category: {
+        type: String,
+        default: null
     }
 }, {
     timestamps: true,
--- src/components/Modals/upload.modal.js
+++ src/components/Modals/upload.modal.js
@@ -1,28 +1,56 @@
 import React from 'react';
-import {ModalBody, ModalHeader, Form, FormGroup, Input, FormText, Button, ModalFooter} from "reactstrap";
+import {ModalBody, ModalHeader, Alert, Form, FormGroup, Input, CustomInput, FormText, Button, ModalFooter} from "reactstrap";
 import Upload from "../../utilities/upload.util";
 
 export default class UploadModal extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            error_msg: ""
+        }
+    }
     handleUploadClick = () => {
-        if (document.getElementById("fileUpload").value !== "") {
-            if (Upload(document.getElementById("fileUpload").files[0])){
+        let file = document.getElementById("fileUpload");
+        let category = document.getElementById("category");
+        if (file.value !== "" && category.value !== "") {
+            if (Upload(document.getElementById("fileUpload").files[0], null, category)){
                 document.getElementById("fileUpload").value = "";
-            }
-            else {
+            } else {
                 alert('Error uploading image.');
             }
         }
         else {
-            alert('Please select an image/gif to upload.');
+            if (category.value === "") this.setState({ error_msg: "Please select a category for your image." });
+            else if (file.value === "") this.setState({ error_msg: "Please select an image to upload." });
         }
     };
 
     render() {
+        const { error_msg } = this.state;
         return (
             <div>
                 <ModalHeader>Upload Image</ModalHeader>
                 <ModalBody>
+                    <Alert color="danger" isOpen={ error_msg !== "" }>
+                        { error_msg }
+                    </Alert>
                     <Form>
+                        <FormGroup>
+                            <CustomInput type="select" name="select" id="category">
+                                <option value="">Select a category...</option>
+                                <option>Animal</option>
+                                <option>Fashion</option>
+                                <option>Food</option>
+                                <option>Funny</option>
+                                <option>Landscape</option>
+                                <option>Nature</option>
+                                <option>Random</option>
+                                <option>Sports</option>
+                                <option>Travel</option>
+                                <option>Vehicle</option>
+                            </CustomInput>
+                        </FormGroup>
                         <FormGroup>
                             <Input id="fileUpload" type="file" accept=".jpg, .png, .gif"/>
                             <FormText color="muted">
