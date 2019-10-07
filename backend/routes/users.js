f7d0cd35d14d87025983461f583814f44e4247c4
Ben Walton <ben.wallos901@gmail.com>
2019-10-07 15:12:39 +1100

    Implement tracking of sock puppet users

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,6 +2,7 @@
 // Package imports
 const router = require('express').Router();
 const jwt = require("jsonwebtoken");
+const requestIP = require("request-ip");
 
 // Middleware imports
 //const auth = require("../../utilities/auth/authMiddleware");
@@ -120,7 +121,8 @@ router.post('/register', (req, res) => {
     const newUser = new User({
         username: username,
         email: email,
-        password: password
+        password: password,
+        last_IP: req.connection.remoteAddress
     });
 
     // Hash password before saving into the database
@@ -169,9 +171,14 @@ router.post('/login', (req, res) => {
             }
         );
 
-        User.findOne({ username }).select('-password').then(user => {
-            res.status(200).json(user);
-        }).catch(err => res.status(400).json(err));
+        user.last_IP = req.connection.remoteAddress;
+
+        user.save()
+            .then(() => {
+                User.findOne({ username }).select('-password').then(user => {
+                    res.status(200).json(user);
+                }).catch(err => res.status(400).json(err));
+            }).catch(err => req.status(400).json(err));      
     })
     .catch(err => res.status(202).json(err));
 });
@@ -231,6 +238,29 @@ router.get("/findUsersByPostCount", (req, res) => {
     User.find().select("username post_count").sort({ post_count: -1 }).limit(10).then(users => {
         res.status(200).json(users);
     }).catch(err => res.status(400).json(err));
+});
+
+router.get("/findSockPuppets", (req, res) => {
+    User.aggregate([{ $match: { is_admin: false, last_IP: { $ne: null } } }])
+        .group({
+            "_id": "$last_IP",
+            "userList": {
+                "$addToSet": {
+                    "username": "$username",
+                    "email": "$email"
+                }
+            }
+        })
+        .project({
+            "_id": 0,
+            "last_IP": "$_id",
+            "userList": 1
+        })
+        .then(userLists => {
+            const listFiltered = userLists.filter(userList => userList.userList.length >= 3);
+            res.status(200).json(listFiltered);
+        })
+        .catch(err => res.status(400).json(err));
 });
 
 module.exports = router;
--- client/src/components/admin.component.js
+++ client/src/components/admin.component.js
@@ -3,34 +3,73 @@ import { BrowserRouter as Router } from 'react-router-dom';
 
 import '../styling.css' ;
 
-import {Button, Input, InputGroup} from "reactstrap";
+import {Button, Table} from "reactstrap";
+import axios from 'axios';
 
 export default class AdminComp extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            resultsVisible: false
+            sockPuppets: []
         }
     }
 
-    toggleResults() {
-
+    componentDidMount() {
+        axios.get("http://localhost:5000/api/users/findSockPuppets")
+            .then(res => {
+                res.data.forEach(userList => {
+                    userList.userList.forEach(user => {
+                        this.setState(prevState => ({
+                            sockPuppets: [...prevState.sockPuppets, 
+                                <tr key={user.username}>
+                                    <td width="20%">{userList.last_IP}</td>
+                                    <td width="30%">{user.username}</td>
+                                    <td width="30%">{user.email}</td>
+                                    <td width="20%"><Button>I'm a Button</Button></td>
+                                </tr>   
+                            ]
+                        }));
+                    });
+                    this.setState(prevState => ({
+                        sockPuppets: [...prevState.sockPuppets,
+                            <tr key={userList.last_IP}>
+                                <td width="20%"></td>
+                                <td width="30%"></td>
+                                <td width="30%"></td>
+                                <td width="20%"></td>
+                            </tr>
+                        ]
+                    }));
+                });;
+            })
+            .catch(err => console.log(err));
     }
 
     render() {
+        const { sockPuppets } = this.state;
         return (
             <Router>
                 <div className="container">
-                    <h1>Administration</h1>
+                    <h1 className="mt-2">Administration</h1>
                     <hr/>
-                    <h3>Search Posts</h3>
-                    <InputGroup>
-                        <Input/>
-                        <Button color={"primary"} onClick={"toggleResults"}>Search</Button>
-                    </InputGroup>
+                    <h3 className="mt-2 mb-2">Potential Sock Puppet Users</h3>
                     <hr/>
-
-                    <h3 style={{paddingTop: "10px"}}>Results</h3>
+                    <div className="mb-4" width="100%">
+                        <Table size="xl">
+                            <thead>
+                                <tr style={{ display: "block" }}>
+                                    <th width="20%">Shared IP Address</th>
+                                    <th width="30%">Username</th>
+                                    <th width="30%">Email Address</th>
+                                    <th width="20%">Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody style={{ display: "block", overflow: "auto", height: "30%", width: "100%"}}>
+                                { sockPuppets }
+                            </tbody>
+                        </Table>
+                    </div>
+                    <h3 className="mt-2 mb-2">Reported Posts</h3>
                     <hr/>
                 </div>
             </Router>
