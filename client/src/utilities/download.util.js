e70882346406bdaffd5c655bef94a8c8ad45388f
Wallos901 <ben.wallos901@gmail.com>
2019-10-08 11:31:51 +1100

    Test if axios proxy is working

--- client/src/utilities/download.util.js
+++ client/src/utilities/download.util.js
@@ -4,7 +4,7 @@ import axios from "axios";
 
 export async function getAll(type, query="") {
     return new Promise((resolve) => {
-        axios.get("http://localhost:5000/api/" + type + query)
+        axios.get("/api/" + type + query)
             .then((response) => {
                 resolve(response.data);
             })
